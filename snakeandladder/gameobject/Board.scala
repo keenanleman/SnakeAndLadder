0686cec22d485379da603d129edefef2a1dc8309
keenanleman <kenl@kenl.name>
2015-11-08 19:06:34 +0700

    Major Update and Additional Test Code:  Board.scala
    Mengimplement GameObjectEvent, mengoverride method update, tile tidak lagi memiliki border dan menambah method untuk memilih tiles berdasarkan nomor tile.
    Menambah test code untuk mencoba kelas player.

--- snakeandladder/gameobject/Board.scala
+++ snakeandladder/gameobject/Board.scala
@@ -10,9 +10,13 @@ import java.awt.{Color, Graphics}
  * @param numRow jumlah baris tile dalam board
  * @param numCol jumlah kolom tile dalam board
  */
-class Board(initialX : Double, initialY : Double, numRow : Int, numCol : Int) extends GameObject(initialX,initialY){
+class Board(initialX : Double, initialY : Double, numRow : Int, numCol : Int) extends GameObject(initialX,initialY) with GameObjectEvent{
   /* multidimensional array dari tile */
   private var tiles : Array[Array[Tile]] = Array.ofDim[Tile](numRow,numCol)
+  private var tilesRefNum : Array[Tile] = new Array[Tile](numRow * numCol)
+
+  /* Test Code (akan di ubah ketika kelas GameState selesai dibuat*/
+  var player : Player = _
 
   /**
    * Method untuk mempopulasi board dengan tile
@@ -31,22 +35,25 @@ class Board(initialX : Double, initialY : Double, numRow : Int, numCol : Int) ex
           currentColor = Tile.TILE_COLOR_EVEN
         }
         tiles(i)(j) = new Tile(currentX,currentY,numOfTiles,currentColor)
+        tilesRefNum(numOfTiles - 1) = tiles(i)(j)
         numOfTiles -= 1
         if(rightToLeft) {
-          currentX += Tile.TILE_SIZE + Tile.TILE_BORDER_THICKNESS
+          currentX += Tile.TILE_SIZE
         }else{
-          currentX -= Tile.TILE_SIZE + Tile.TILE_BORDER_THICKNESS
+          currentX -= Tile.TILE_SIZE
         }
       }
-      currentY += Tile.TILE_SIZE + Tile.TILE_BORDER_THICKNESS
+      currentY += Tile.TILE_SIZE
       if(rightToLeft){
-        currentX -= Tile.TILE_SIZE + Tile.TILE_BORDER_THICKNESS
+        currentX -= Tile.TILE_SIZE
       }else{
-        currentX += Tile.TILE_SIZE + Tile.TILE_BORDER_THICKNESS
+        currentX += Tile.TILE_SIZE
       }
       rightToLeft = !rightToLeft
     }
   }
+
+  def getTileByNumber(number : Int) : Tile = tilesRefNum(number - 1)
 
   /**
    * Method render dari board, merender tile-tile yang dibutuhkan board
@@ -58,6 +65,17 @@ class Board(initialX : Double, initialY : Double, numRow : Int, numCol : Int) ex
         tiles(i)(j).render(graphics)
       }
     }
+    player.render(graphics)
+  }
+
+  /**
+   * Method update pada board akan mengupdate semua GameObject yang merupakan
+   * komposisi dari pada Kelas board
+   */
+  override def update(): Unit = {
+    /* Test Code (akan di ubah ketika kelas GameState selesai dibuat*/
+    player.update()
+    player.moveToTile(tiles(0)(9))
   }
 }
 

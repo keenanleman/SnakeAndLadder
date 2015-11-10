577f785acd120cd6553f67ff17fbee2ca9b53ad7
Mirza Lazuardi <lazuardist@hotmail.com>
2015-11-10 19:55:10 +0700

    update class Dice

--- snakeandladder/gameobject/Dice.scala
+++ snakeandladder/gameobject/Dice.scala
@@ -10,13 +10,29 @@ class Dice(initialX : Double, initialY : Double) extends GameObject(initialX, in
   x+= Dice.DICE_POSTITION
   y+= Dice.DICE_POSTITION
 
+  /**
+    * nilai pada mata dadu
+    */
   private var currentValue : Int = 1
+  /**
+    * delayer untuk merubah nilai dadu
+    */
   private var diceUpdateDelayer	: Int = 0
+  /**
+    * delayer untuk mengocok dadu
+    */
   private var rollDelayer : Int = 0
+  /**
+    * nilai dadu sudah di kocok atau belum
+    */
   private var rolled : Boolean = false
-
+  /**
+    * variable untuk menggambar dadu
+    */
   private var diceDrawable : RoundRectangle2D.Double = new RoundRectangle2D.Double(x,y,Dice.DICE_SIZE,Dice.DICE_SIZE,20,20)
-
+  /**
+    * nilai dadu yang di random
+    */
   private val randomDice = new Random(System.nanoTime())
 
   def roll : Int = {
@@ -29,6 +45,9 @@ class Dice(initialX : Double, initialY : Double) extends GameObject(initialX, in
     return currentValue
   }
 
+  /**
+    * method untuk update nilai dadu setelah di kocok
+    */
   override def update () : Unit = {
     if(rolled){
       rollDelayer += 1
@@ -41,6 +60,10 @@ class Dice(initialX : Double, initialY : Double) extends GameObject(initialX, in
     }
   }
 
+  /**
+    * @param graphics graphics dari canvas pada kelas GameDisplay
+    * method untuk merender dadu
+    */
   override def render(graphics: Graphics): Unit ={
     var g2d : Graphics2D = graphics.asInstanceOf[Graphics2D]
     var diceColor = new Color(Color.RED.getRed, Color.RED.getGreen, Color.RED.getBlue, 200)

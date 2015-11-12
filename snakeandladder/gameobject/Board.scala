5a175141acdddb6c9ba2505859e5a2209c2dea31
keenanleman <kenl@kenl.name>
2015-11-12 16:26:31 +0700

    Major Update: Board.scala
    Memindahkan tanggung jawab untuk merender komponen yang bukan bagian dari board ke GameState

--- snakeandladder/gameobject/Board.scala
+++ snakeandladder/gameobject/Board.scala
@@ -1,7 +1,13 @@
 package snakeandladder.gameobject
 
-import java.awt.{Color, Graphics}
+import java.awt.event.MouseEvent
+import java.awt.geom.Point2D
+import java.awt.{Shape, Color, Graphics}
+import java.util.ArrayList
 import java.util.Random
+import java.util.Iterator
+
+import snakeandladder.gameevent.MouseEventListener
 
 /**
  * Kelas board, representasi dari papan permainan, kelas ini mepopulasi tile sesuai dengan masukan
@@ -11,71 +17,32 @@ import java.util.Random
  * @param numRow jumlah baris tile dalam board
  * @param numCol jumlah kolom tile dalam board
  */
-class Board(initialX : Double, initialY : Double, numRow : Int, numCol : Int) extends GameObject(initialX,initialY) with GameObjectUpdate{
+class Board(initialX : Double, initialY : Double, numRow : Int, numCol : Int) extends GameObject(initialX,initialY) with GameObjectUpdate with MouseEventListener{
   /* multidimensional array dari tile */
   private var tiles : Array[Array[Tile]] = Array.ofDim[Tile](numRow,numCol)
   private var tilesRefNum : Array[Tile] = new Array[Tile](numRow * numCol)
-
-  /* Test Code (akan di ubah ketika kelas GameState selesai dibuat*/
-  private var dice : Dice = new Dice(numRow * Tile.TILE_SIZE,0)
-
   /**
-    * Random generator
-    */
+   * Random generator
+   */
   private val random : Random = new Random(System.nanoTime())
 
   /**
-    * Array untuk menyimpan object Snake
-    */
-  private var snakes : Array[Snake] = new Array[Snake](Board.numSnake)
+   * Array untuk menyimpan object Snake
+   */
+  private var snakes : Array[Snake] = null 
 
-  /* Test Code (akan di ubah ketika kelas GameState selesai dibuat*/
-  var player : Player = _
+  private var dice : Dice = null
+
+  private val players : ArrayList[Player] = new ArrayList[Player]
+  private val playerIterator : Iterator[Player] = players.iterator()
 
 
   /* Menginisiasi board */
   initBoard
-
-  /**
-    * Method untuk menginisiasi Board
-    */
-  def initBoard : Unit = {
-    initTiles
-    initSnake
-    player = new Player(getTileByNumber(1),this)
-    player.moveToTile(tiles(0)(9))
-  }
-
-  /**
-    * Method untuk mempopulasi board dengan tile
-    */
-  private def initSnake : Unit = {
-    var from : Int = 0
-    var to : Int = 0
-    var maxIndex = (numRow * numCol) + 1
-    for(i <- 0 until Board.numSnake){
-      from = 1 + random.nextInt(maxIndex - 1)
-      while(getTileByNumber(from).isHasSnake){
-        from = 1 + random.nextInt(maxIndex - 1)
-      }
-      var fromTile : Tile = getTileByNumber(from)
-      fromTile.gotSnake
-
-      to = 1 + random.nextInt(maxIndex - 1)
-      while(to >= from || getTileByNumber(to).isHasSnake){
-        to = 1 + random.nextInt(maxIndex - 1)
-      }
-      var toTile : Tile = getTileByNumber(to)
-      toTile.gotSnake
-      snakes(i) = new Snake(fromTile, toTile)
-      fromTile.setSnakeHead(snakes(i))
-    }
-  }
-
   /**
    * Method untuk mempopulasi board dengan tile
    */
-  private def initTiles : Unit = {
+  private def initBoard : Unit = {
     var currentX = x
     var currentY = y
     var numOfTiles = numRow * numCol
@@ -107,12 +74,56 @@ class Board(initialX : Double, initialY : Double, numRow : Int, numCol : Int) ex
     }
   }
 
+  def turnPlayer : Unit = {
+    dice.roll
+  }
+
   /**
     * Mengembalikan tile berdasarkan nomor tile
     * @param number nomor tile
     * @return tile yang dipilih berdasarkan nomor tile
     */
   def getTileByNumber(number : Int) : Tile = tilesRefNum(number - 1)
+
+  def getDice : Dice = dice
+
+  def getPlayers : Array[Player] = players.toArray(new Array[Player](players.size()))
+
+  def getSnakes : Array[Snake]  = snakes
+
+  def setDice(dice : Dice) : Unit = {
+    this.dice = new Dice(numRow * Tile.TILE_SIZE,0)
+  }
+  def addPlayer(player : Player) : Unit = {
+    players.add(player)
+  }
+
+  /**
+   * Method untuk mempopulasi board dengan tile
+   */
+  def populateSnake(numOfSnake : Int) : Unit = {
+    snakes = new Array[Snake](numOfSnake)
+    var from : Int = 0
+    var to : Int = 0
+    var maxIndex = (numRow * numCol) + 1
+    for(i <- 0 until numOfSnake){
+      from = 1 + random.nextInt(maxIndex - 1)
+      while(getTileByNumber(from).isHasSnake){
+        from = 1 + random.nextInt(maxIndex - 1)
+      }
+      var fromTile : Tile = getTileByNumber(from)
+      fromTile.gotSnake
+
+      to = 1 + random.nextInt(maxIndex - 1)
+      while(to >= from || getTileByNumber(to).isHasSnake){
+        to = 1 + random.nextInt(maxIndex - 1)
+      }
+      var toTile : Tile = getTileByNumber(to)
+      toTile.gotSnake
+      snakes(i) = new Snake(fromTile, toTile)
+      fromTile.setSnakeHead(snakes(i))
+    }
+  }
 
   /**
    * Method render dari board, merender tile-tile yang dibutuhkan board
@@ -125,27 +136,26 @@ class Board(initialX : Double, initialY : Double, numRow : Int, numCol : Int) ex
         tiles(i)(j).render(graphics)
       }
     }
-    for(i <- 0 until Board.numSnake){
-      snakes(i).render(graphics)
+  }
+
+  override def triggeredMouseEvent(event: MouseEvent): Unit = {
+    if(dice.getDrawable.contains(new Point2D.Double(event.getX,event.getY))){
+      if(MouseEvent.getMouseModifiersText(event.getModifiersEx).equals("Button1")){
+        turnPlayer
+      }
     }
-    player.render(graphics)
-    dice.render(graphics)
   }
 
   /**
-   * Method update pada board akan mengupdate semua GameObject yang merupakan
-   * komposisi dari pada Kelas board
+   * Method update yang harus di implementasikan semua GameObject yang mampu mengupdate
+   * data
    */
   override def update(): Unit = {
-    /* Test Code (akan di ubah ketika kelas GameState selesai dibuat*/
-    player.update()
+    if(dice.getCurrentValue != 0){
+      players.get(0).moveToTile(getTileByNumber(players.get(0).getCurrentTile + dice.getCurrentValue))
+      dice.resetDice
+    }
   }
 }
 
 
-object Board{
-  /**
-   * Banyak ular dalam sebuah board
-   */
-  val numSnake : Int = 5
-}
